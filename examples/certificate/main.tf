45e5f92efcbcc1b3dd5901a16e92d350e359626f
awet <avvvet@gmail.com>
2025-06-05 04:55:17 -0230

    feat: add certificate implementation, service matching and example usage

---
+++ examples/certificate/main.tf
@@ -0,0 +1,50 @@
+# ===================================================================
+# Example certificate upload and service matching
+# ===================================================================
+
+
+terraform {
+  required_providers {
+    cachefly = {
+      source  = "cachefly.com/avvvet/cachefly" # todo: cachefly/cachefly
+      version = "0.1.0"
+    }
+  }
+}
+
+provider "cachefly" {
+  api_token = ""
+}
+
+
+resource "cachefly_certificate" "web-east-2" {
+  certificate = <<-EOT
+-----BEGIN CERTIFICATE-----
+MIIDjTCCAxOgAwIBAgISBnT/I/7K68WFPfoulTHn8uPjMAoGCCqGSM49BAMDMDIx
+Fg==
+-----END CERTIFICATE-----
+  EOT
+
+  certificate_key = <<-EOT
+-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgV5NnlUadOM1q1fEm
+-----END PRIVATE KEY-----
+  EOT
+}
+
+
+resource "cachefly_service" "yellow_web_app" {
+  name               = "yellow-web-app"
+  unique_name        = "yellow-web-app-dev"
+  description        = "Web application CDN for example.com"
+  auto_ssl           = true  
+  configuration_mode = "API_RULES_AND_OPTIONS"
+}
+
+
+resource "cachefly_service_domain" "app_main" {
+  service_id      = cachefly_service.yellow_web_app.id
+  name            = "example.com"  # This matches our certificate!
+  description     = "Main website domain"
+  validation_mode = "DNS"
+}
